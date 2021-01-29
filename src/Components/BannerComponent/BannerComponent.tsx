b24b34c36f0399160f3c57fec036ce6da769aeef
Konrad Duleba <kon.duleba@gmail.com>
2021-01-29 21:45:25 +0100

    Added banner and colors to index.css

---
+++ src/Components/BannerComponent/BannerComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TheFacebookBanner from '../../Assets/the-facebook-banner.jpg';
+import TheFacebookLogo from '../../Assets/the-facebook-logo.jpg';
+import './BannerComponent.scss';
+
+const BannerComponent = ():JSX.Element => (
+    <section className='banner-wrapper'>
+        <img src={TheFacebookBanner} alt="the facebook banner"/>
+        <div className='logo-and-menu'>
+            <img src={TheFacebookLogo} alt="the facebook logo"/>
+            <ul className='menu'>
+                <li>
+                    <a href="#login">login</a>
+                </li>
+                <li>
+                    <a href="#register">register</a>
+                </li>
+                <li>
+                    <a href="#about">about</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+)
+
+export default BannerComponent;
--- src/Components/MainPageComponent/MainPage.tsx
+++ src/Components/MainPageComponent/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import BannerComponent from '../BannerComponent/BannerComponent';
 import LoginPage from '../LoginPageComponent/LoginPage';
 import './MainPage.scss';
 
 const App = ():JSX.Element => {
   return (
     <div className="main-page-wrapper">
+      <BannerComponent/>
       <h1>the facebook</h1>
       <LoginPage />
     </div>
